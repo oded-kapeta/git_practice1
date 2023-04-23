8f9b7995b0145b77afaa0a017aec2a1d79e87706
oded-kapeta <oded.kapeta@campus.technion.ac.il>
2023-04-23 22:11:57 +0300

    new practice

---
+++ src/git_practice_class.java
@@ -0,0 +1,7 @@
+public class git_practice_class {
+    public static void main(String[] args) {
+        for(int i = 0 ; i < 20 ;i ++){
+            System.out.println("hisssdvkl");
+        }
+    }
+}

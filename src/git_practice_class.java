68e3b6ae590db85de7d9d4cf761e33b4d0dd02c9
yuval-belelovsky <ybelelovsky@campus.technion.ac.il>
2023-04-24 11:48:09 +0300

    add func2

--- src/git_practice_class.java
+++ src/git_practice_class.java
@@ -3,6 +3,13 @@ public class git_practice_class {
         for(int i = 0 ; i < 20 ;i ++){
             System.out.println("hisssdvkl+jnvjksdbjksnjk+jhjnjn+more code");
             //jjjjj
+
         }
+        System.out.println(power3(3));
+
+    }
+    public static int power3(int x)
+    {
+        return x*x*x;
     }
 }

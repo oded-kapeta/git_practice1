7855cc80689de8553d61b03af32d6f02033919ad
yuval-belelovsky <ybelelovsky@campus.technion.ac.il>
2023-04-24 11:41:17 +0300

    added code

--- src/git_practice_class.java
+++ src/git_practice_class.java
@@ -1,7 +1,7 @@
 public class git_practice_class {
     public static void main(String[] args) {
         for(int i = 0 ; i < 20 ;i ++){
-            System.out.println("hisssdvkl+jnvjksdbjksnjk+jhjnjn");
+            System.out.println("hisssdvkl+jnvjksdbjksnjk+jhjnjn+code");
             //jjjjj
         }
     }
